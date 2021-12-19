1dbc9541912ca170a3083227e471662680680916
AnoshSohail <anoshsohail99@gmail.com>
2021-12-19 21:42:43 +0500

    Intro component with signup form.

---
+++ intro-component-with-signup-form-master/js/script.js
@@ -0,0 +1,38 @@
+function validation() {
+  let fName = document.getElementById("fName").value;
+  let lName = document.getElementById("lName").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let errorIcon = document.getElementById("errorIcon");
+  let regex =
+    /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z0-9a-zA-Z]+)\.([a-zA-z]){2,7}$/;
+  let str = email;
+  if (fName == "") {
+    document.getElementById("firstName-error").innerHTML =
+      "First Name cannot be empty";
+    document.getElementById("fName").style.border =
+      "2px solid hsl(0, 100%, 74%)";
+    document.getElementById("errorIcon").style.display = "inline";
+  }
+  if (lName == "") {
+    document.getElementById("lastName-error").innerHTML =
+      "Last Name cannot be empty";
+    document.getElementById("lName").style.border =
+      "2px solid hsl(0, 100%, 74%)";
+  }
+  if (!regex.test(str)) {
+    document.getElementById("email-error").innerHTML =
+      "Looks like this is not an email";
+    document.getElementById("email").style.border =
+      "2px solid hsl(0, 100%, 74%)";
+    document.getElementById("email").style.color = " hsl(0, 100%, 74%)";
+    document.getElementById("email").style.fontWeight = "600";
+  }
+  if (password == "") {
+    document.getElementById("password-error").innerHTML =
+      "password cannot be empty";
+    document.getElementById("password").style.border =
+      "2px solid hsl(0, 100%, 74%)";
+  }
+  return false;
+}
